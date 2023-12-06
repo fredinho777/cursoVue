2ca7f068293427a3133be3c71631f173d4cefd6a
fredinho777 <fredinho777@hotmail.com>
2023-12-06 17:40:27 -0500

    Component props

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,12 +1,27 @@
 export default {
     template:`
-        <button class="bg-gray-200 hover:gray-400 border rounded px-4 py-2 disabled:cursor-not-allowed" :disabled="processing">
+        <button 
+            :class="{
+                'border rounded px-4 py-2 disabled:cursor-not-allowed':true,
+                'bg-blue-600 hover:bg-blue-700':type == 'primary',
+                'bg-red-600 hover:bg-red-700':type == 'secondary',
+                'bg-gray-600 hover:bg-gray-700':type == 'muted',
+                'is-loading': processing
+            }" 
+            :disabled="processing"
+        >
             <slot/>
         </button>
     `,
-    data() {
-        return {
-            processing: false
-        };
+
+    props:{
+        type: {
+            type: String, 
+            default: 'primary'
+        },
+        processing: {
+            type: Boolean,
+            default: false
+        }
     }
 }
